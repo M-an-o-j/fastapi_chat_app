8fa38709a619c158a38071252b6a7dc6ec463615
M-an-o-j <ronaldomano7070@gmail.com>
2024-04-21 16:17:44 +0530

    restricted two connections

--- main.py
+++ main.py
@@ -15,20 +15,25 @@ class ConnectionManager:
     # accept and append the connection to the list
     async def connect(self, websocket: WebSocket, room_id: int):
         await websocket.accept()
-        print(len(self.active_rooms), room_id)
-        if room_id in self.active_rooms and len(self.active_rooms[room_id]) >= 2:
-            self.disconnect(websocket)
 
+        if room_id in self.active_rooms and len(self.active_rooms[room_id]) >= 2:
+            # If there are already two connections in the room, reject the new connection
+            await websocket.close()
+            return "Room is full. You can't connect."
+
+
+        # Append the websocket to the list
         if room_id not in self.active_rooms:
             self.active_rooms[room_id] = []  # Initialize the list if the key doesn't exist
-        self.active_rooms[room_id].append(websocket)  # Append the websocket to the list
+        self.active_rooms[room_id].append(websocket)
 
     # remove the connection from list
     def disconnect(self, websocket: WebSocket):
         print("|hgjyh")
         for room_id, connections in self.active_rooms.items():
+            print(connections,"connctions")
             if websocket in connections:
-                connections.remove(websocket)
+                connections.remove(websocket)   
                 if not connections:  # If no more connections in the room, remove the room
                     del self.active_rooms[room_id]
                 break
@@ -59,18 +64,19 @@ def read_index(request: Request):
 async def websocket_endpoint(websocket: WebSocket, room_id: int):
     # accept connections
     await connectionmanager.connect(websocket, room_id)
-    print(websocket.__dict__)
+    # print(websocket.__dict__)
     print(connectionmanager.active_rooms)
+    # await websocket.accept()
     try:
         while True:
             # receive text from the user
             data = await websocket.receive_text()
-            await connectionmanager.send_personal_message(f"You : {data}", websocket)
+            # await connectionmanager.send_personal_message(f"You : {data}", websocket)
             # broadcast message to the connected user
             await connectionmanager.broadcast(f"Client #{room_id}: {data}",room_id, websocket)
 
     # WebSocketDisconnect exception will be raised when client is disconnected
-    except WebSocketDisconnect:
+    except (WebSocketDisconnect,Exception) as e:
         connectionmanager.disconnect(websocket)
         # Broadcast a message indicating that the client has left the chat
         for room_id, connections in connectionmanager.active_rooms.items():
