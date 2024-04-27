af1ae89cd23911d9a738e0468095a513e26bf67b
M-an-o-j <ronaldomano7070@gmail.com>
2024-04-27 21:31:17 +0530

    Added private chat using name to send message

--- main.py
+++ main.py
@@ -6,32 +6,25 @@ from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-
 class ConnectionManager:
     # initialize list for websockets connections
     def __init__(self):
         self.active_rooms: dict = {}
 
     # accept and append the connection to the list
-    async def connect(self, websocket: WebSocket, room_id: int):
+    async def connect(self, websocket: WebSocket, name: str):
+        print(self.active_rooms)
         await websocket.accept()
 
-        if room_id in self.active_rooms and len(self.active_rooms[room_id]) >= 2:
-            # If there are already two connections in the room, reject the new connection
-            await websocket.close()
-            return "Room is full. You can't connect."
-
-
         # Append the websocket to the list
-        if room_id not in self.active_rooms:
-            self.active_rooms[room_id] = []  # Initialize the list if the key doesn't exist
-        self.active_rooms[room_id].append(websocket)
+        if name not in self.active_rooms:
+            self.active_rooms[name] = websocket  
+            print(self.active_rooms)
 
     # remove the connection from list
     def disconnect(self, websocket: WebSocket):
-        print("|hgjyh")
         for room_id, connections in self.active_rooms.items():
-            print(connections,"connctions")
+            # print(connections,"connctions")
             if websocket in connections:
                 connections.remove(websocket)   
                 if not connections:  # If no more connections in the room, remove the room
@@ -40,6 +33,7 @@ class ConnectionManager:
 
     # send personal message to the connection
     async def send_personal_message(self, message: str, websocket: WebSocket):
+        print(message)
         await websocket.send_text(message)
 
     # send message to the list of connections
@@ -60,20 +54,22 @@ def read_index(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
 
-@app.websocket("/ws/{room_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: int):
+@app.websocket("/ws/{name}")
+async def websocket_endpoint(websocket: WebSocket,name:str):
     # accept connections
-    await connectionmanager.connect(websocket, room_id)
-    # print(websocket.__dict__)
-    print(connectionmanager.active_rooms)
-    # await websocket.accept()
+    await connectionmanager.connect(websocket, name)
     try:
         while True:
             # receive text from the user
+            receiver = await websocket.receive_text()
             data = await websocket.receive_text()
-            # await connectionmanager.send_personal_message(f"You : {data}", websocket)
+            if receiver in connectionmanager.active_rooms:
+                user = connectionmanager.active_rooms[receiver]
+            else:
+                await websocket.send_text("user not found")
+            await connectionmanager.send_personal_message(f"{name} : {data}", user )
             # broadcast message to the connected user
-            await connectionmanager.broadcast(f"Client #{room_id}: {data}",room_id, websocket)
+            # await connectionmanager.broadcast(f"Client #{room_id}: {data}",room_id, websocket)
 
     # WebSocketDisconnect exception will be raised when client is disconnected
     except (WebSocketDisconnect,Exception) as e:
@@ -83,4 +79,3 @@ async def websocket_endpoint(websocket: WebSocket, room_id: int):
             if websocket in connections:
                 await connectionmanager.broadcast(f"Client #{room_id} left the chat", room_id, websocket)
                 break
-
